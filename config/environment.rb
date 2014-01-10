77c8cf8f609d622d16873648da8758aa73440c04
tvergilio <thalita.vergilio@yahoo.co.uk>
2014-01-10 14:37:25 +0000

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialise Timezone
+require 'tzinfo'
+
+# Initialize the Rails application.
+Rails.application.initialize!
